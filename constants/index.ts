12339ffccfccbc2fc030783ea74a4c02d252a414
Mbonyii <mbonyiiinaguel@gmail.com>
2025-11-04 01:17:08 +0200

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,18 @@
 
+export const APP_CONFIG = {
+  name: 'ALX Listing App',
+  description: 'Airbnb clone for property listings',
+  version: '1.0.0'
+};
+
+export const API_ENDPOINTS = {
+  properties: '/api/properties',
+  bookings: '/api/bookings'
+};
+
+export const UI_TEXT = {
+  bookNow: 'Book Now',
+  viewDetails: 'View Details',
+  loading: 'Loading...',
+  noProperties: 'No properties available'
+};
